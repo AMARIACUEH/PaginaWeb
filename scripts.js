126d1dee5cc4e718f62824cda32f63d815b4b58c
AnMa CueH <anacuellar_29@hotmail.com>
2023-11-02 21:15:36 -0500

    Agrego  mi pagina web

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function mostrar() {
+    var container = document.getElementsByClassName("contenido")[0];
+        
+    if(container.style.display == "none"){
+        container.style.display = "block";
+    }else{
+        container.style.display = "none";
+    }
+
+
+}
